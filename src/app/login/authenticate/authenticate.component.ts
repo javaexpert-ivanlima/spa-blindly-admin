b5aca72a4f9782bd319cdb86d7baa148732ae3be
ivan.marques.lima@gmail.com <ivan.marques.lima@gmail.com>
2021-02-21 19:14:19 -0300

    adicionado component login

--- src/app/login/authenticate/authenticate.component.ts
+++ src/app/login/authenticate/authenticate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-authenticate',
@@ -10,6 +11,7 @@ export class AuthenticateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $("#overlayblocked").addClass('overlay');
   }
 
 }
